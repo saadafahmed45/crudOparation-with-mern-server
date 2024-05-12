a6d9630255c8296037d56635a4bd60476249b094
saadafahmed45 <hafijnahid45@.com>
2024-05-13 01:13:12 +0530

    aaded

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
     app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await userCollection.findOne(query);
+      const result = await usersCollection.findOne(query);
       res.send(result);
     });
 
